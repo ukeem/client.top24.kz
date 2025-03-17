fbaba0f85ff1f056873ade5783fb4842d1a4e4b4
ukeem <ikim.trader@gmail.com>
2025-03-17 20:34:58 +0500

    1

--- src/ui/components/Nav/index.tsx
+++ src/ui/components/Nav/index.tsx
@@ -4,5 +4,6 @@ import NavWrap from '../NavWrap'
 
 export default async function Nav() {
 	const categories = await getAllCategories() // ✅ Асинхронный вызов только здесь
+
 	return <NavWrap categories={categories} />
 }
