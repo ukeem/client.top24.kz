8e2a75f9c9eaebeccbe81804a52cdedf0f46628c
ukeem <ikim.trader@gmail.com>
2025-03-17 20:45:42 +0500

    1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { getPostsByCategoryLimit } from '@/services/api';
 import MainPostList from '@/ui/components/MainPostList';
 
-export const revalidate = 60;
+export const revalidate = 3600
 
 export default async function Home() {
 
--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,7 +2,7 @@
 
 import { MetadataRoute } from "next";
 
-export const revalidate = 3600;
+// export const revalidate = 3600;
 // export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
