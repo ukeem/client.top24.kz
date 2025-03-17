f05e49ec00515d092821eda73f02e7f3abc0aadc
ukeem <ikim.trader@gmail.com>
2025-03-17 15:36:49 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,7 +6,6 @@ export const revalidate = 60;
 export default async function Home() {
 
 	const posts = await getPostsByCategoryLimit()
-	// console.log("📌 Загруженные посты:", posts);
 
 	return (
 		<>
