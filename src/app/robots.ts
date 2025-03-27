4df83682d293249e884183fd79970275c06576fc
ukeem <ikim.trader@gmail.com>
2025-03-27 12:59:23 +0500

    Update robots.ts

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -6,13 +6,23 @@ import { MetadataRoute } from "next";
 // export const dynamic = "force-static";
 
 export default function robots(): MetadataRoute.Robots {
+    const siteUrl = process.env.NEXT_PUBLIC_SITE || "https://top24.kz";
+
+    if (!process.env.NEXT_PUBLIC_SITE) {
+        console.warn(
+            "NEXT_PUBLIC_SITE не задан, используется значение по умолчанию."
+        );
+    }
+
     return {
-        rules: {
-            userAgent: "*",
-            allow: "/",
-        },
-        sitemap: `${
-            process.env.NEXT_PUBLIC_SITE || "https://top24.kz"
-        }/sitemap.xml`,
+        rules: [
+            {
+                userAgent: "*",
+                allow: "/",
+                disallow: "/images",
+            },
+        ],
+        sitemap: `${siteUrl}/sitemap.xml`,
+        host: siteUrl,
     };
 }
